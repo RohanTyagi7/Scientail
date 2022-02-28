a7c2460ead6a1aa5538175c3747f450e769274ab
RohanTyagi7 <96214239+RohanTyagi7@users.noreply.github.com>
2022-02-28 16:14:04 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+class JavaCode
+{
+    static main(args)
+    {
+        var input = java.util.Scanner(java.lang.System.in);
+        var name = input.nextLine();
+        console.log(name);
+    }
+}
+JavaCode.main([]);
